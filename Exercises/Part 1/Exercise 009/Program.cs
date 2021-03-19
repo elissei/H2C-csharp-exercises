e0a749066930e0b777e9a88ff79ee0731ce2463f
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-19 04:11:10 +0200

    19.3.2021

--- Exercises/Part 1/Exercise 009/Program.cs
+++ Exercises/Part 1/Exercise 009/Program.cs
@@ -6,7 +6,11 @@ namespace exercise_09
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.WriteLine("Hello, how are you?");
+            string line1 = Console.ReadLine();
+            Console.WriteLine("That's interesting, tell me more");
+            string line2 = Console.ReadLine();
+            Console.WriteLine("Thank you for sharing!");
 
         }
     }
