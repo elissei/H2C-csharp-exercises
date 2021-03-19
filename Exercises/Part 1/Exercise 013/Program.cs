89b42509a8f0844425692a319c91e9288aeea4e8
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-19 04:36:44 +0200

    19.3.2021

--- Exercises/Part 1/Exercise 013/Program.cs
+++ Exercises/Part 1/Exercise 013/Program.cs
@@ -6,7 +6,10 @@ namespace exercise_13
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give a number!");
+            string number = Console.ReadLine();
+            double value = Convert.ToDouble(number);
+            Console.WriteLine("You gave " + value);
 
     }
   }
