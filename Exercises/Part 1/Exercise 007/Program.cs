40daffff26377c13d048c744f3fb20f5585e8849
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-19 03:59:13 +0200

    19.3.2021

--- Exercises/Part 1/Exercise 007/Program.cs
+++ Exercises/Part 1/Exercise 007/Program.cs
@@ -7,8 +7,10 @@ namespace exercise_07
         public static void Main(string[] args)
         {
             Console.WriteLine("Give input!");
-            // Write your code here:
-            
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.WriteLine(input);
+            Console.WriteLine(input);
         }
     }
 }
