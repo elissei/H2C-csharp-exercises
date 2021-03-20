c159050f8c3ec3b661159403046d7043a37e4de3
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-20 23:40:09 +0200

    20.3.2021

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            String num1 = Console.ReadLine();
+            Console.WriteLine("Give the second number!");
+            String num2 = Console.ReadLine();
+            Console.WriteLine("Give the third number!");
+            String num3 = Console.ReadLine();
+            int int1 = Convert.ToInt32(num1);
+            int int2 = Convert.ToInt32(num2);
+            int int3 = Convert.ToInt32(num3);
+            int result = int1 + int2 + int3;
+            Console.WriteLine(result);
 
     }
   }
