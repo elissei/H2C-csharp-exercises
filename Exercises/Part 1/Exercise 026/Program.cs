220e75ee754b1c530b7b70305224c0c0121b4f55
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 00:54:44 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 026/Program.cs
+++ Exercises/Part 1/Exercise 026/Program.cs
@@ -6,8 +6,14 @@ namespace exercise_26
   {
     public static void Main(string[] args)
     {
-            
+            Console.WriteLine("Give your age:");
+            String age = Console.ReadLine();
+            int num = Convert.ToInt32(age);
+            if (num < 1900)
+            {
+                Console.WriteLine("You're old!");
             }
+            
 
     }
   }
