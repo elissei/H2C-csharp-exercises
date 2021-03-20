652634d8799bed9111fbb0934d7ee21f34fdde52
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 00:43:33 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 025/Program.cs
+++ Exercises/Part 1/Exercise 025/Program.cs
@@ -6,7 +6,13 @@ namespace exercise_25
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give a number:");
+            String number = Console.ReadLine();
+            int num = Convert.ToInt32(number);
+            if (num == 1984)
+            {
+                Console.WriteLine("Orwell");
+            }
 
     }
   }
