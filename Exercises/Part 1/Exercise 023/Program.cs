31f75d6b291377edf9ec5ba76344669078b35bb4
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 00:36:09 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            String num1 = Console.ReadLine();
+            Console.WriteLine("Give the second number!");
+            String num2 = Console.ReadLine();
+            int int1 = Convert.ToInt32(num1);
+            int int2 = Convert.ToInt32(num2);
+            int sum = int1 + int2;
+            int multiply = int1 * int2;
+            int subtract = int1 - int2;
+            int divide = int1 / int2;
+            Console.WriteLine(int1 + "+" + int2 + "=" + sum);
+            Console.WriteLine(int1 + "-" + int2 + "=" + subtract);
+            Console.WriteLine(int1 + "*" + int2 + "=" + multiply);
+            Console.WriteLine(int1 + "/" + int2 + "=" + divide);
 
     }
   }
