239b5843e7e76eee1ffbdee0d0279933e68a85a2
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-20 22:56:30 +0200

    20.3.2021

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,7 +7,21 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give a string:");
+            String word = Console.ReadLine();
+            Console.WriteLine("Give an integer:");
+            String number = Console.ReadLine();
+            Console.WriteLine("Give a double:");
+            String decimal23 = Console.ReadLine();
+            Console.WriteLine("Give a boolean:");
+            String boo = Console.ReadLine();
+            int number1 = Convert.ToInt32(number);
+            double deci = Convert.ToDouble(decimal23);
+            Boolean boo1 = Convert.ToBoolean(boo);
+            Console.WriteLine("Your string: " + word);
+            Console.WriteLine("Your integer: " + number1);
+            Console.WriteLine("Your double: " + deci);
+            Console.WriteLine("Your boolean: " + boo1);
 
     }
   }
