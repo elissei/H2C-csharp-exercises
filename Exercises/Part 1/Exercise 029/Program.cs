e4f7801c40a101ce5f5e95796a0dd96c49527be6
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 01:04:39 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,8 +6,24 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+            Console.WriteLine("Give the first number!");
+            String int1 = Console.ReadLine();
+            Console.WriteLine("Give the second number!");
+            String int2 = Console.ReadLine();
+            int num1 = Convert.ToInt32(int1);
+            int num2 = Convert.ToInt32(int2);
+            if (num1 > num2)
+            {
+                Console.WriteLine("The larger number is " + num1 + "!");
+            }
+            if (num1 < num2)
+            {
+                Console.WriteLine("The larger number is " + num2 + "!");
+            }
+            else
+            {
+                Console.WriteLine("They are equal!");
+            }
     }
   }
 }
