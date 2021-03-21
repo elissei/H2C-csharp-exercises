e9513190c22c5caaa94e747134d803de49aeb3d4
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 02:54:18 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Give a number:");
+                string input = Console.ReadLine();
+                int num = Convert.ToInt32(input);
+                if (num == 0)
+                {
+                    break;
+                }
+                else if (num < 0)
+                {
+                    sum += 1;
+                }
+
+                 
+            }
+            Console.WriteLine("Total amount of negative numbers: " + sum);
 
     }
   }
