fa713eb775115fdac404a081aeb8c2dbdca7a217
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 02:59:13 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -6,8 +6,27 @@ namespace exercise_40
   {
     public static void Main(string[] args)
     {
+            int sum = 0;
+            int amount = 0;
+            while (true)
+            {
+                Console.WriteLine("Give a number:");
+                string input = Console.ReadLine();
+                int num = Convert.ToInt32(input);
+                if (num == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    sum += num;
+                    amount += 1;
+                }
 
-      // Write your code here:
+                 
+            }
+            Console.WriteLine("Total sum of numbers: " + sum);
+            Console.WriteLine("Total amount of numbers: " + amount);
     }
   }
 }
