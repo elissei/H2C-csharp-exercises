a9063cbe2524d43c94751670e6fa29b362432dde
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 04:03:34 +0200

    21.3.2021

--- Exercises/Part 2/Exercise 043/Program.cs
+++ Exercises/Part 2/Exercise 043/Program.cs
@@ -6,7 +6,17 @@ namespace exercise_43
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int num = Convert.ToInt32(Console.ReadLine());
+            int outcome = 0;
+            if (num < 0)
+            {
+                outcome = num * -1;
+                Console.WriteLine(outcome);
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
       
     }
   }
