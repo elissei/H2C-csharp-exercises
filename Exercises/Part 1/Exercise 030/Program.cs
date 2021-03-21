dcc61c39efa5914a7af189cb6478630a941731a4
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 02:14:48 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,41 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-
+            Console.WriteLine("Give your percent [0 - 100]:");
+            string input = Console.ReadLine();
+            int percent = Convert.ToInt32(input);
+            if (percent < 0)
+            {
+                Console.WriteLine("Impossible");
+            }
+            else if (percent < 50)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (percent < 60)
+            {
+                Console.WriteLine("Grade: 1");
+            }
+            else if (percent < 70)
+            {
+                Console.WriteLine("Grade: 2");
+            }
+            else if (percent < 80)
+            {
+                Console.WriteLine("Grade: 3");
+            }
+            else if (percent < 90)
+            {
+                Console.WriteLine("Grade: 4");
+            }
+            else if (percent < 100)
+            {
+                Console.WriteLine("Grade: 5");
+            }
+            else
+            {
+                Console.WriteLine("Outstanding!");
+            }
     }
   }
 }
