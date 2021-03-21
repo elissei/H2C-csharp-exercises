eede98fce8f0393f201ce0b7cc8fa48aa159e8a8
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 03:55:35 +0200

    21.3.2021

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -6,8 +6,11 @@ namespace exercise_42
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int sum = number1 + number2;
+            double squareRoot = Math.Sqrt(sum);
+            Console.WriteLine(squareRoot);
     
     }
   }
