4a04d0dae8b8322a39a9078a3a4c07f0e9eae8ca
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-21 02:45:51 +0200

    21.3.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,7 +6,28 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            while (true)
+            {
+                Console.WriteLine("Give a number:");
+                string input = Console.ReadLine();
+                int num = Convert.ToInt32(input);
+                int num1 = 0;
+                if (num == 0)
+                {
+                    break;
+                }
+                else if (num % 2 != 0)
+                {
+                    Console.WriteLine("That is negative");
+                }
+                else
+                {
+                    num1 = num * num;
+                    Console.WriteLine(num1);
+                }
+
+                 
+            }
 
     }
   }
