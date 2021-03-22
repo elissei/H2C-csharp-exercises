8eb5fd8fc80efb2e07cd45739700cd6f4d239e44
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-22 03:50:16 +0200

    22.3.2021

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -7,11 +7,23 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
+            int x = Convert.ToInt32(Console.ReadLine());
+            PrintUntilNumber(x);
+      
 
     }
 
     // Write your method here:
+        public static void PrintUntilNumber(int number)
+        {
+            int i = 1;
+            while (i < number)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
 
   }
 }
+
