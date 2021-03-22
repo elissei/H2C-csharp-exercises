b524ca61a15122d80aff2df47e079e8d8e3ddaa0
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-22 02:42:20 +0200

    22.3.2021

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,12 +7,20 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
+            Console.WriteLine("How many times?");
+            int i = Convert.ToInt32(Console.ReadLine());
+            
  
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase(int i) {
+            int x = 0;
+            while (x < i) 
+            { 
+                Console.WriteLine("In a hole in the ground there lived a method");
+                x++;
+                }
     }
   }
 }
