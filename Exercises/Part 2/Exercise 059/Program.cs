bf13f0fa9bb099576cb259dbdae97d6a7bb7dfe1
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-28 00:56:41 +0200

    28.3.2021

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -6,11 +6,26 @@ namespace exercise_59
   {
     public static void Main(String[] args)
     {
-      int answer = Greatest(2, 7, 3);
+      int answer = GrEatest(2, 7, 3);
       Console.WriteLine("Greatest: " + answer);
+            Console.ReadKey();
     }
 
-    // Write your method here:
+    public static int GrEatest(int number1, int number2, int number3)
+        {
+            if(number1 > number2 && number1 > number3)
+            {
+                return number1;
+            }
+            if(number2 > number1 && number2 > number3)
+            {
+                return number2;
+            }
+            else
+            {
+                return number3;
+            }
+        }
 
   }
 }
