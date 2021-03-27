c08b42a1cb99c4772b8034c8a957520baf042d5a
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-28 01:24:56 +0200

    28.3.2021

--- Exercises/Part 2/Exercise 060/Program.cs
+++ Exercises/Part 2/Exercise 060/Program.cs
@@ -6,32 +6,58 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
-
+            //PrintStars(5);
+            //PrintStars(3);
+            //PrintStars(9);
+            //PrintSquare(20);
+            //PrintRectangle(17, 3);
+            PrintTriangle(4);
+            Console.ReadKey();       
+            
     }
 
 
     public static void PrintStars(int number)
     {
-      // you can print one star with the command
-      // Console.Write("*");
-      // call the print command n times
-      // in the end print a line break with the comand
-      // Console.WriteLine("");
+            int i = 0;
+            while(i < number)
+            {
+                Console.Write("*");
+                i++;
+            }
+            Console.WriteLine("");
     }
 
     public static void PrintSquare(int size)
     {
-
+            int i = 0;
+            while(i < size)
+            {
+                PrintStars(size);
+                i++;
+            }
     }
 
     public static void PrintRectangle(int width, int height)
     {
-
+            int i = 0;
+            while(i < height)
+            {
+                PrintStars(width);
+                i++;
+            }
     }
 
     public static void PrintTriangle(int size)
     {
-
+            int w = 1;
+            int i = 0;
+            while(i < size)
+            {
+                PrintStars(w);
+                w++;
+                i++;
+            }
     }
   }
 }
