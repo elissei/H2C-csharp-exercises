c99c7b7662ad04b966033c9c91ea939920e9ace2
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-03-30 23:16:41 +0300

    30.3.2021

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,27 +6,58 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
+            //PrintStars(4);
+            //PrintRightTriangle(40);
+            ChristmasTree(40);
+            Console.ReadKey();
 
     }
 
     public static void PrintStars(int number)
     {
-
+            int i = 0;
+            while(i < number)
+            {
+                Console.Write("*");
+                i++;
+            }
+            Console.WriteLine("");
     }
 
     public static void PrintSpaces(int number)
     {
-
+            int x = 0;
+            while(x < number)
+            {
+                Console.Write(" ");
+                x++;
+            }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+            int i = 1;
+            while (i <= size) 
+            {
+                PrintSpaces(size - i);
+                PrintStars(i);
+                i++;
+             }
     }
 
     public static void ChristmasTree(int height)
     {
+            int i = 1;
+            while (i <= height) {
+            PrintSpaces(height - i);
+            PrintStars(i + (i - 1));
+            i++;
+            }
 
+            PrintSpaces(height - 2);
+            PrintStars(3);
+            PrintSpaces(height - 2);
+            PrintStars(3);
     }
   }
 }
