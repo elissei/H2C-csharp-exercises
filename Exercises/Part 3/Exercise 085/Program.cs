773277d9ec50b842531f148c031d400b5d724100
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-17 02:44:35 +0300

    17.4.2021

--- Exercises/Part 3/Exercise 085/Program.cs
+++ Exercises/Part 3/Exercise 085/Program.cs
@@ -7,7 +7,19 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
-
+        Console.WriteLine("Enter a username: ");
+        string un = Console.ReadLine();
+        Console.WriteLine("Enter password: ");
+        string pas = Console.ReadLine();
+        if((un == "alex" && pas == "sunshine") || (un == "emma" && pas == "haskell"))
+            {
+                Console.WriteLine("You have successfully logged in!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect username or password!");
+            }
+           Console.ReadKey();
 
     }
   }
