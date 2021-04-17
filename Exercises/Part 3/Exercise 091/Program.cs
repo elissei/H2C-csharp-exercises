4bb7354593c6bf79f2b7cf64d37422c8886d1905
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-18 01:16:24 +0300

    18.4.2021

--- Exercises/Part 3/Exercise 091/Program.cs
+++ Exercises/Part 3/Exercise 091/Program.cs
@@ -7,7 +7,28 @@ namespace exercise_91
   {
     public static void Main(string[] args)
     {
-
+            int oldest = 0;
+            string name = "";
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input == "")
+                {
+                    break;
+                }
+                string[] parts = input.Split(',');
+                int age = Convert.ToInt32(parts[1]);
+                for (int i = 0; i < parts.Length; i++) 
+			    {
+                    if(age > oldest)
+                    {
+                        oldest = age;
+                        name = parts[0];
+                    }
+			    }
+            }
+            Console.WriteLine("Name of the oldest: " + name);
+            Console.ReadKey();
     }
   }
 }
