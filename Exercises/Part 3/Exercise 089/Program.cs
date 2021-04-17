ed998cdc965dace2e6b0c505da7d8295ef3ae665
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-17 03:26:59 +0300

    17.4.2021

--- Exercises/Part 3/Exercise 089/Program.cs
+++ Exercises/Part 3/Exercise 089/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
-
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input == "")
+                {
+                    break;
+                }
+                string[] parts = input.Split();
+                Console.WriteLine(parts[parts.Length - 1]);
+                Console.ReadKey();
+            }
 
     }
   }
