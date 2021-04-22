c952c51c7507672adc9431ec9c7d8beef90d7885
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-23 00:50:08 +0300

    23.4.2021

--- Exercises/Part 4/Exercise 115/Program.cs
+++ Exercises/Part 4/Exercise 115/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
-
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input == "end")
+                {
+                    break;
+                }
+                int x = Convert.ToInt32(input);
+                int z = x * x * x;
+                Console.WriteLine(z);
+            }
+            Console.ReadKey();
     }
   }
 }
