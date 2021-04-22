554007f4219dda9a84f3e4187f5fc6b700c92443
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-22 15:06:02 +0300

    22.4.2021

--- Exercises/Part 4/Exercise 110/Program.cs
+++ Exercises/Part 4/Exercise 110/Program.cs
@@ -8,6 +8,23 @@ namespace exercise_110
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+        List<Item> items = new List<Item>();
+
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                if(name == "")
+                {
+                    break;
+                }
+                items.Add(new Item(name));
+            }
+            foreach(Item item in items)
+            {
+                Console.WriteLine(item);               
+            }
+            Console.ReadKey();
     }
   }
 }
