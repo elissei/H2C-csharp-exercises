c9801622d0245f7cd581cb34340e680362f4b182
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-22 15:27:27 +0300

    22.4.2021

--- Exercises/Part 4/Exercise 111/Program.cs
+++ Exercises/Part 4/Exercise 111/Program.cs
@@ -9,7 +9,25 @@ namespace exercise_111
     {
       // IMPLEMENT YOUR CODE IN HERE!
       List<PersonalInformation> list = new List<PersonalInformation>();
-
+            while (true)
+            {
+                Console.WriteLine("First name:");
+                string fname = Console.ReadLine();
+                if(fname == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Last name:");
+                string lname = Console.ReadLine();
+                Console.WriteLine("Identification number:");
+                string id = Console.ReadLine();
+                list.Add(new PersonalInformation(fname, lname, id));
+            }
+            foreach(PersonalInformation person in list)
+            {
+                Console.WriteLine(person.firstName + " " + person.lastName);
+            }
+            Console.ReadKey();
     }
   }
 }
