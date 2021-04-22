85b31df2de9a07ec0d6104a1f4bb1338f9fa205e
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-22 20:41:53 +0300

    22.4.2021

--- Exercises/Part 4/Exercise 112/Program.cs
+++ Exercises/Part 4/Exercise 112/Program.cs
@@ -8,9 +8,32 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
-
-
+      List<TelevisionProgram> list = new List<TelevisionProgram>();
+            while (true)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                 
+                if(name == "")
+                {
+                    break;
+                }
+                Console.Write("Duration: ");
+                int duration = Convert.ToInt32(Console.ReadLine());
+                list.Add(new TelevisionProgram(name, duration));
+            }
+            Console.Write("Program's maximum duration? ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            foreach(TelevisionProgram televisionProgram in list)
+            {
+                if(televisionProgram.duration <= max)
+                {
+                    Console.WriteLine(televisionProgram);
+                }
+            }
+            Console.ReadKey();
     }
+        
   }
 }
 
