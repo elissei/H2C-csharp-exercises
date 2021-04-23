3b9cadba76286ec552e2769a170c7bc8d4517bef
Elissei Hilakivi <elissei.hilakivi@centria.fi>
2021-04-24 00:00:51 +0300

    24.4.2021

--- Exercises/Part 5/Exercise 124/Program.cs
+++ Exercises/Part 5/Exercise 124/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace exercise_124
 {
@@ -7,6 +8,15 @@ namespace exercise_124
     public static void Main(string[] args)
     {
       // Implement your program here!
+      List<string> lines = new List<string>();
+      lines.Add("Sometimes programming really frustrates me");
+      lines.Add("But somehow, I just get over it");
+      lines = null;
+      foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+      Console.ReadKey();
 
       
     }
